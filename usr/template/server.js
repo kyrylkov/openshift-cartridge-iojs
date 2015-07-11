25ffe0c73a37edfed2963c11ad39e57050022ecf
kyrylkov <kyrylkov@gmail.com>
2015-07-11 15:13:48 +0300

    show installed versions

--- usr/template/server.js
+++ usr/template/server.js
@@ -2,12 +2,14 @@
 
 let http = require('http');
 
-const IP = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const PORT = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 http.createServer(function (req, res) {
+  let body = 'Welcome to io.js on OpenShift!\n\nEverything seems to be in order.\n\nRunning io.js:';
+  for (let v in process.versions) { body += `\n- ${v}: ${process.versions[v]}`; }
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end(body);
 }).listen(PORT, IP, function() {
   console.log(`Listening on ${IP}:${PORT}`);
 });
